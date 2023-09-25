59bdb51b67daa95700a0d46c268a830135943eed
KDJLBryant <kbryant6644@gmail.com>
2023-09-25 19:33:26 +0000

    Bit of Task 2
    Incomplete Task 2 code. Got the class up and running and object creating. just need to utilize the validity checker and converter operational!

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -4,16 +4,90 @@ using System.Runtime.CompilerServices;
 
 namespace ConsoleApp1
 {   
+    public class WeatherData
+    {
+        public double Temp { get; set; }
+        public int Humid { get; set; }
+        public string Scale { get; set; }
+
+        public WeatherData(double temp, int humid, string scale)
+        {
+            Temp = temp;
+            Humid = humid;
+            Scale = scale;
+        }
+
+        public bool CheckValidTemp()
+        {
+            if (Scale == "C")
+            {
+                if (Temp < -60 || Temp > 60)
+                {
+                    Console.WriteLine("This temperature is not valid!");
+                    return false;
+                }
+                else
+                {
+                    return true;
+                }
+            }
+            else
+            {
+                if (Temp < -76 || Temp > 140)
+                {
+                    Console.WriteLine("This temperature is not valid!");
+                    return false;
+                }
+                else
+                {
+                    return true;
+                }
+            }
+        }
+        public bool CheckValidHumidity()
+        {
+            if (Humid < 0 || Humid > 100)
+            {
+                Console.WriteLine("This humidity is not valid!");
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+        
+        public double Convert()
+        {
+            if (Scale == "C")
+            {
+                Console.Write("C to F is - ");
+                Temp = (Temp * 9 / 5) + 32;
+                Scale = "F";
+                return Temp;
+            }
+            else
+            {
+                Console.Write("F to C is - ");
+                Temp = (Temp - 32) * 5 / 9;
+                Scale = "C";
+                return Temp;
+            }
+        }
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
+            // TASK 1 -----
+
             // Initialise variables
             List<int> numbers = new List<int>();
             int index = 0;
-            bool loop = true;
+            bool loopOne = true;
 
-            while (loop)
+            while (loopOne)
             {
                 // Get user input
                 Console.Write($"Number {index++}: ");
@@ -37,8 +111,23 @@ namespace ConsoleApp1
                         Console.WriteLine(i);
                     }
                     Console.WriteLine($"Biggest = {numbers.Max()} / Smallest = {numbers.Min()}");
-                    loop = false;
+                    loopOne = false;
                 }
+            }
+            // TASK 2 -----
+            bool loopTwo = true;
+
+            while (loopTwo)
+            {
+                Console.WriteLine("\nEnter Weather info - ");
+                Console.Write("Enter temp Scale: ");
+                string setScale = Console.ReadLine();
+                Console.Write("Enter Temp: ");
+                double setTemp = double.Parse(Console.ReadLine());
+                Console.Write("Enter Humidity: ");
+                int setHumid = int.Parse(Console.ReadLine());
+
+                WeatherData weatherData = new WeatherData(setTemp, setHumid, setScale);
             }
         }
     }
